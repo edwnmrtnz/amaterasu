2cc413b9a60d858892f06cf8f8761074d97492a0
Edwin Martinez <dev.edwinmartinez@gmail.com>
2023-05-14 09:28:57 +0800

    Add testingutils module

---
+++ testingutils/README.md
@@ -0,0 +1,3 @@
+# testingutils
+
+Provide a single dependency for all testing dependency and utils needs.
---
+++ testingutils/src/test/java/com/github/amaterasu/testingutils/MoviesTest.kt
@@ -0,0 +1,38 @@
+
+package com.github.amaterasu.testingutils
+
+import com.google.common.truth.Truth.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
+import org.mockito.Mockito
+
+
+class MoviesTest {
+
+    data class Movie(val id: String, val name: String)
+
+    interface MoviesRepository {
+        fun fetch(params: Map<String, String>): List<Movie>
+    }
+
+    class FetchMoviesUseCase(private val repository: MoviesRepository) {
+        fun execute(page: Int): List<Movie> {
+            return repository.fetch(mapOf("page" to page.toString(), "status" to "active"))
+        }
+    }
+
+    @Test
+    fun `should only fetch active movies`() {
+        val repo = Mockito.mock(MoviesRepository::class.java)
+        val sut = FetchMoviesUseCase(repo)
+
+        sut.execute(page = 1)
+
+        val captor = argumentCaptor<Map<String, String>>()
+        Mockito.verify(repo).fetch(capture(captor))
+        val params = captor.value
+        assertThat(params["status"]).isEqualTo("active")
+    }
+
+
+}
