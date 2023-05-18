0732a8c3f3acf4a95e86643ccab83d2f4680475d
Edwin Martinez <dev.edwinmartinez@gmail.com>
2023-05-18 22:23:51 +0800

    Add ability to scoped custom class like non-VM presenters using scopey

---
+++ scopey/src/main/java/com/github/amaterasu/scopey/Scopey.kt
@@ -0,0 +1,50 @@
+package com.github.amaterasu.scopey
+
+import androidx.lifecycle.get
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+
+inline fun <reified T> ViewModelStoreOwner.scopey(noinline creator: () -> T): Lazy<T> {
+    return LazyScopedValue({ viewModelStore }, { ScopyViewModel.Factory(creator) })
+}
+
+class ScopyViewModel<V>(
+    val value: V
+) : ViewModel() {
+    class Factory<V>(val valueFactory: () -> V) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            ScopyViewModel(valueFactory()) as? T
+                ?: throw java.lang.IllegalArgumentException("Unknown type")
+    }
+}
+
+class LazyScopedValue<T : Any>(
+    private val storeProducer: () -> ViewModelStore,
+    private val factoryProducer: () -> ViewModelProvider.Factory
+) : Lazy<T> {
+    private var cached: Any = NotSet
+
+    @Suppress("UNCHECKED_CAST")
+    override val value: T
+        get() {
+            val value = cached
+            return if (value == NotSet) {
+                val factory = factoryProducer()
+                val store = storeProducer()
+                val viewModel = ViewModelProvider(store, factory).get<ScopyViewModel<T>>()
+
+                viewModel.value.also { cached = it }
+            } else {
+                value as T
+            }
+        }
+
+    override fun isInitialized() = cached != NotSet
+
+    companion object {
+        private val NotSet = Any()
+    }
+}
