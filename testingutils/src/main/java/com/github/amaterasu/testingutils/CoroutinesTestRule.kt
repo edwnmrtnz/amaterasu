25ebd448180a80afaf602dcdc0b3b1e4a1eb2ce3
Edwin Martinez <dev.edwinmartinez@gmail.com>
2023-05-24 07:45:05 +0800

    Add coroutine test rule

---
+++ testingutils/src/main/java/com/github/amaterasu/testingutils/CoroutinesTestRule.kt
@@ -0,0 +1,27 @@
+package com.github.amaterasu.testingutils
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CoroutineTestRule : TestWatcher() {
+
+    val dispatcher = UnconfinedTestDispatcher()
+    val scope = CoroutineScope(dispatcher)
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
